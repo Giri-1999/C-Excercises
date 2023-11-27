8682aa299c38c320727acbbd4a02e2d24d0fb90b
Giri-1999 <machhitadaka@gmail.com>
2023-11-27 11:30:29 +0530

    Adding the files excluding bin and obj

---
+++ HelloFromCSharp/Giri.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloFromCSharp
+{
+    internal class Giri
+    {
+        //C# Program to Print Odd Numbers in a Given Range
+        public static void OddNumbers()
+        {
+            Console.WriteLine("Enter the Number : ");
+            int range = int.Parse(Console.ReadLine());
+            for (int i = 1; i < range; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+        //C# Program to Find the Largest of Three Numbers
+        public static void LargestOfThree()
+        {
+            int x = 10, y = 20, z = 5;
+            if(x>y && x > z)
+            {
+                Console.WriteLine($"{x} is the Largest Number");
+            }else if(y>x && y > z)
+            {
+                Console.WriteLine($"{y} is the Largest Number");
+            }
+            else
+            {
+                Console.WriteLine($"{z} is the Largest Number");
+            }
+        }
+        // C# Program to Swap Two Numbers with and without 3rd variable.
+        public static void Swap()
+        {
+            int a = 10, b = 20;
+            Console.WriteLine($"Beafore swapping a={a} b={b}");
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"After swapping a={a} b={b}");
+        }
+        //C# Program to Find the Sum of All the Multiples of 3 and 5 under a givem limit number.
+        public static void SumOfAllMultiples()
+        {
+            Console.WriteLine("Enter the Number : ");
+            int range = int.Parse(Console.ReadLine());
+            int sum=0;
+            for (int i = 1; i<range; i++) 
+            {
+                if(i%3==0 || i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        //C# Program to Print All the Multiples of 17 which are Less than 100
+        public static void Multiple()
+        {
+            for(int i = 1; i< 100; i++)
+            {
+                if (i % 17 == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        //C# Program to Find Sum of Digits of a Number
+        public static void SumOfDigits()
+        {
+            Console.WriteLine("Enter the Number : ");
+            int digit= int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (digit > 0)
+            {
+                sum = sum + digit%10;
+                digit /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloFromCSharp;
+
+Console.WriteLine("Enter the Number for Executing the Program : \n\n" +
+    "1.Print Odd Numbers in a Given Range:\n\n" +
+    "2.Find the Largest of Three Numbers:\n\n" +
+    "3.Swap Two Numbers with and without 3rd variable:\n\n" +
+    "4.Find the Sum of All the Multiples of 3 and 5 under a givem limit number:\n\n" +
+    "5.Print All the Multiples of 17 which are Less than 100:\n\n" +
+    "6.Find Sum of Digits of a Number:\n");
+int Number = int.Parse(Console.ReadLine());
+switch (Number)
+{
+    case 1:
+        Giri.OddNumbers();
+        break;
+    case 2:
+        Giri.LargestOfThree();
+        break;
+    case 3:
+        Giri.Swap();
+        break;
+    case 4:
+        Giri.SumOfAllMultiples();
+        break;
+    case 5:
+        Giri.Multiple();
+        break;
+    case 6:
+        Giri.SumOfDigits();
+        break;
+    default:
+        Console.WriteLine("Invalid input, Please enter the valid data");
+        break;
+}
+
+
